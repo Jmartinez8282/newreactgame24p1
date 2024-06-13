93bae4cae772450d24b4f063b03f781ad18fcdc9
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-06-13 14:05:02 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `'nav' 'main'`,
+          lg: `'nav nav' 'aside main'`, //1024
+        }}
+      >
+        <GridItem area="nav" >
+       <NavBar/>
+        </GridItem>
+        <Show above="lg">
+        <GridItem area="aside" bg="pink">
+          Aside
+        </GridItem>
+
+        </Show>
+        <GridItem area="main" bg="green">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
+};
+
+export default App;
